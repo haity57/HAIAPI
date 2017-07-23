5faba5ac394295f56cca9989bd140dc69c9dd7ef
haity57 <nguyenhaijp5792@gmail.com>
2017-07-23 23:30:02 +0900

    Request success

--- RestaurantsAPI/Restaurants.cs
+++ RestaurantsAPI/Restaurants.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
 namespace RestaurantsAPI
 {
     public class Restaurants
     {
+        const string ApiURL = "http://api.gnavi.co.jp/RestSearchAPI/20150630/";
+
+        const string keyid = "4b459c82d6c82a40f653dfd1c031f70c";
+
         public Restaurants()
         {
         }
+
+		private async Task<string> HttpGetAsync(string url)
+		{
+			HttpClient httpClient = new HttpClient();
+
+			Task<string> getStringTask = httpClient.GetStringAsync(url);
+
+			string result = await getStringTask;
+
+			return result;
+		}
+
+        public JObject getRestaurantsByCoordinates(double lati/* tude */, double longi/* tude */)
+        {
+
+            string URL = ApiURL + "?keyid=" + keyid + "&format=json" + "&latitude=" + lati + "&longitude" + longi + "&range=5"; // Range = 1||2||3||4||5 500-3000m
+			string json = "";
+
+			Task task = new Task(() =>
+			{
+				json = HttpGetAsync(URL).Result;
+			});
+			task.Start();
+			task.Wait();
+
+			return JObject.Parse(json);
+        }
+
+
     }
 }
--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -16,13 +16,18 @@ namespace RestaurantsAPI.iOS
         {
             base.ViewDidLoad();
 
+            // Call API
+            Restaurants restaurants = new Restaurants();
+            string test = restaurants.getRestaurantsByCoordinates(35.6915, 139.7081).ToString();
+
             // Perform any additional setup after loading the view, typically from a nib.
             Button.AccessibilityIdentifier = "myButton";
             Button.TouchUpInside += delegate
             {
                 var title = string.Format("{0} clicks!", count++);
-                Button.SetTitle(title, UIControlState.Normal);
-            };
+				// Button.SetTitle(title, UIControlState.Normal);
+                Button.SetTitle(test, UIControlState.Normal);
+			};
         }
 
         public override void DidReceiveMemoryWarning()
